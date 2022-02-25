86d5f5494a166a4456671f765ee38ec64040793a
William Marshall <w.marshall@elsevier.com>
2022-02-25 15:21:45 +0000

    Add user_input method

--- directory.rb
+++ directory.rb
@@ -27,6 +27,23 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty?
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
