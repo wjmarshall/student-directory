bcbfeb74b95175635f5171675b449bdc5b960309
William Marshall <w.marshall@elsevier.com>
2022-02-28 10:44:56 +0000

    Add save_students method

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -31,6 +32,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -57,6 +60,16 @@ def show_students
   print_header
   print_students_list
   print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
